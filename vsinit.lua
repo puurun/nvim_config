4db74cad5f99cd75ea9c0e7092e207672174548e
Seokmin Woo <clematis7593@gmail.com>
2024-05-26 14:34:39 +0900

    vscode update

--- vsinit.lua
+++ vsinit.lua
@@ -3,4 +3,9 @@ require('lazy_setup')
 require('plugins/basic')
 require('after/keymaps')
 
+local vscode = require('vscode-neovim')
 
+vim.keymap.set("i", "<C-c>", function() 
+  vim.cmd('stopinsert')
+  vscode.action('workbench.action.files.save')
+end)
